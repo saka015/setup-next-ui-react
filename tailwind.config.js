70c965c9599f379bce4a692503f59588e1847ddb
Kamal Nayan <sknasylum8454@gmail.com>
2024-03-29 01:53:52 +0530

    --nextui setup done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import { nextui } from "@nextui-org/react";
+
+export default {
+  content: [
+    // ...
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
